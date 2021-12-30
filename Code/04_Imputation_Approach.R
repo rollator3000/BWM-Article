202030d2f84c6aa08614ca8359a2e53374d6c068
rollator3000 <kuchenmann93@gmail.com>
2021-12-30 16:06:39 +0100

    added corrected TOBMI-function to code/04, added corrected seeds for evaluation & cleaned up the script generally. Evaluation ready to run

--- Code/04_Imputation_Approach.R
+++ Code/04_Imputation_Approach.R
@@ -1,14 +1,18 @@
 "Script to evaluate the Imputation approach on data with blockwise missingness
+ Split a DF to a train- & test-set, separately induce block-wise missingness
+ patterns to these and evaluate the IMP-Approach on these then.
+ This is done 5x for each DF in './Data/Raw/' for each possible combination 
+ of blockwise missingess patterns in train- & test-set.
 
-  > 1. Impute thes missing values in the train-set with TOBMI
-       (see 'https://academic.oup.com/bioinformatics/article/35/8/1278/5092930')
-  > 2. Remove all blocks from train that are not available in test
-  > 3. Fit a RF on the remaining train-set
-  > 4. Use this RF to predict on the test-set & get the corresponding metrics
-  
-  > Functions for the imputation with TOMBI have been supplied by Dr. Roman Hornung.
+ IMP-Approach:
+  > Impute the missing values in the train-set with TOBMI
+    (see 'https://academic.oup.com/bioinformatics/article/35/8/1278/5092930'
+     - the functions for TOBMI were supplied by Dr. Roman Hornung)
+  > Remove all blocks from the train-set that are not available in the test-set
+  > Fit a RF on the remaining train-set & create predicitons for the test-set
+  > Evaluate the results with common metrics (AUC, Accuracy, F-1 Score, ...)
 "
-# [0] Set WD, load packages, define fix variables and functions                ----
+# [0] Set WD, load packages, define variables and functions                  ----
 # 0-1 Set WD
 setwd("/Users/frederik/Desktop/BWM-Article/")             # Mac
 setwd("C:/Users/kuche/Desktop/BWM-Paper")                 # Windows
@@ -22,33 +26,37 @@ library(doParallel)
 library(caret)
 library(pROC)
 
-# 0-3 Define fixed variables
-# 0-3-1 Define amount of usable cores (parallel computing)
-detectCores()
-registerDoParallel(cores = 2)
+# 0-3 Define variables
 
-# 0-4 Load functions from 'code/01_Create_BWM_Pattern"
+# 0-4 Define functions
+# 0-4-1 Load functions from 'code/01_Create_BWM_Pattern"
 source("./Code/01_Create_BWM_Pattern.R")
 
-# 0-5 Define functions
-# 0-5-1 Altered TOBMI function with greater speed than the original TOMBI
+# 0-4-2 Altered TOBMI function with greater speed than the original TOMBI
+#       (supplied by Dr. Hornung)
 TOBMIfast <- function(x = cpg, y = exp) {
   
-  ##Calculating the distances among un-/complete cases using auxiliary dataset
+  # Calculating the distances among un-/complete cases using auxiliary dataset
   dist.matrix <- as.matrix(dist( x ))
   
-  ##Neighbors list for every uncomplete cases
+  # Neighbors list for every uncomplete cases
   missing_num <- length(which(complete.cases(y) == F)) 
   donors <- list()
   for(i in 1:missing_num){
-    donors[[i]] <- as.matrix(sort(dist.matrix[i,c(c(missing_num + 1):dim(x)[1])])[1 : floor(sqrt(dim(x)[1] - missing_num))])
-    ## NEW: If the Mahalanobis distance was zero, the weights were NaN. --> Replace
-    ## weights of zero by the smallest observed distance greater than zero:
-    donors[[i]][,1][donors[[i]][,1]==0] <- min(donors[[i]][,1][donors[[i]][,1]!=0])
+    tempobj <- dist.matrix[i,c(c(missing_num + 1):dim(x)[1])]
+    donors[[i]] <- as.matrix(tempobj[order(tempobj, runif(length(tempobj)))][1 : floor(sqrt(dim(x)[1] - missing_num))])
+    # NEW: If the Mahalanobis distance was zero, the weights were NaN. --> Replace
+    #      distances of zero by the smallest observed distance greater than zero.
+    # Exception: Sometimes all distances were zero. In these cases just set all 
+    #            distances to 1, which corresponds to equal weights.
+    if (any(donors[[i]][,1]!=0))
+      donors[[i]][,1][donors[[i]][,1]==0] <- min(donors[[i]][,1][donors[[i]][,1]!=0])  
+    else
+      donors[[i]][,1] <- 1
   }
   
   ##Neighbors will be weighted by distance 
-  donors.w<-list()		
+  donors.w<-list()    
   for(i in 1:missing_num){
     donors.w[[i]]<-(1/donors[[i]][,1])/sum((1/donors[[i]][,1]))
   }
@@ -64,7 +72,8 @@ TOBMIfast <- function(x = cpg, y = exp) {
   imputed.data<-y
 }
 
-# 0-5-2 Function to do imputation a la TOMBI
+
+# 0-4-2 Function to do imputation a la TOBMI (supplied by Dr. Hornung)
 ImputeWithTOBMI <- function(omicsdata, blockind) {
   
   rownamessafe        <- rownames(omicsdata)
@@ -83,7 +92,7 @@ ImputeWithTOBMI <- function(omicsdata, blockind) {
   return(omicsdata)
 }
 
-# 0-5-3 HelpFunction for 0-5-1
+# 0-4-3 HelpFunction for 0-4-2 (supplied by Dr. Hornung)
 ImputeTwo <- function(omicsdatacompl, omicsdatamiss) {
   
   reorderind <- order(complete.cases(omicsdatamiss))
@@ -95,30 +104,25 @@ ImputeTwo <- function(omicsdatacompl, omicsdatamiss) {
   return(imputed)
 }
 
-# 0-5-4 Fit a RF on train & get the predicitons on test
+# 0-4-4 Fit a RF on train & get predictions on test
 get_predicition <- function(train, test) {
-  " Get predictions from a RF-Model for 'test', whereby the RF is trained on 'train'..
-    Important: > All obs. in 'test' are fully observed!
-               > 'train' only consits of features that are availabe in 'test'
-                  & contains not a single NA.
-                  
-      --> Train a RF on 'train' & use this to generate predicitons for 'test' then.
+  "Fit a RF on 'train' and create predicitons for 'test' then. 
                 
     Args:
-      - train  (DF): DF that only contains variables that are also availabe for 'test'.
+      - train  (DF): DF that only contains variables also availabe for 'test'.
                      Must not contain any NA values! 
-      - test   (DF): DF that is completly observed for all its observations
+      - test   (DF): DF that is completly observed in all observations
                      
     Return:
       - List with: > 'pred_classes' = predicted class for each observation in 'test'
-                   > 'pred_prob_pos_class' = predicted probability for a obs. 
+                   > 'pred_prob_pos_class' = predicted probability for each obs. 
                                              to be in class 1
                    > settings of the RF for 'mtry', 'min_node_size' & 'ntree'
   "
   # [0] Check Inputs
-  # 0-1 'train' & 'test' must be dataframes w/o missing values
+  # 0-1 'train' & 'test' must be DFs w/o missing values
   assert_data_frame(train, any.missing = FALSE)
-  assert_data_frame(test, min.rows = 1, any.missing = FALSE)
+  assert_data_frame(test, any.missing = FALSE)
   
   # 0-2 'train' must not contain any colnames not avaible in 'test' & vic versa
   if (!all((colnames(train) %in% colnames(test)))) {
@@ -138,15 +142,15 @@ get_predicition <- function(train, test) {
   #     (define response & use remaining variables as features)
   formula_all <- as.formula(paste('ytarget', " ~ ."))
   
-  # --3 Fit the actual RF (only use standard-settings)
+  # --3 Fit the RF (use standard-settings)
   RF <- rfsrc(formula = formula_all, data = train, samptype = "swr", 
               seed = 12345678, var.used = 'all.trees')
   
-  # 1-2 Get Prediciton on the testset from the RF
+  # 1-2 Get Prediction for the test-set
   predicitons <- predict(RF, test)
   
-  # 1-3 Return the predicted classes & the predicted probabilites for class '1'
-  #     aswell as the settings of the RF
+  # 1-3 Return the predicted classes & the predicted probabilities for class '1'
+  #     as well as the settings of the RF
   return(list('pred_classes'        = predicitons$class,
               'pred_prob_pos_class' = predicitons$predicted[,'1'],
               'RF_ntree'            = RF$ntree,
@@ -155,17 +159,16 @@ get_predicition <- function(train, test) {
 }
 
 # 0-5-5 Evaluate the imputation approach
-evaal_imp_approach <- function(path = './Data/Raw/BLCA.Rda', frac_train = 0.75, split_seed = 1312,
-                               block_seed_train = 1234, block_seed_test = 1312, train_pattern = 2, 
-                               train_pattern_seed = 12, test_pattern = 2) {
+eval_imp_approach <- function(path = './Data/Raw/BLCA.Rda', frac_train = 0.75, split_seed = 1312,
+                              block_seed_train = 1234, block_seed_test = 1312, train_pattern = 2, 
+                              train_pattern_seed = 12, test_pattern = 2) {
   "Evaluate the Imputation-Approach on the data 'path' points to.
    The (block-wise) missing values in the train-set are imputed with TOMBI. Then all the blocks that
-   are not available for the test-set are then removed from the train-set. A RF is then fit on the
-   reamining train-set & used to predict on the test-set then. Based on the predicitions for the 
-   test-set, various metrics are calculated.
-   Finally return a DF with the the AUC, the Brier-Score and  the standard metrics Precision, Recall,
-   Sensitivity, Specificity, F-1 Score & Accuracy + all the settings for the evaluation 
-   (e.g. path, seeds, train_pattern, settings for RF, block_order, ...).
+   are not available for the test-set are removed from the train-set.
+   If the train-set is 'empty' afterwards, the approach can not be appliedm! Else a RF is 
+   then fit on the reamining train-set & used to predict on the test-set. Evaluate the predicitons
+   with common metrics, and return all results in a DF w/ all the settings for the evaluation 
+   (e.g. path, seeds, train_pattern, settings for RF, ...)
    
    Args:
       > path               (str): Path to a dataset - must contain 'Data/Raw'
@@ -185,7 +188,7 @@ evaal_imp_approach <- function(path = './Data/Raw/BLCA.Rda', frac_train = 0.75, 
   "
   # [0] Check Inputs
   #     --> All arguments are checked in the functions 'get_train_test()' &
-  #         'get_predicition()' that werde loaded from 'Code/01_Create_BWM_Pattern.R'
+  #         'get_predicition()' that were loaded from 'Code/01_Create_BWM_Pattern.R'
   
   # [1] Load the data & prepare them for the Imputation-Approach
   # 1-1 Load the data from 'path', split it to test- & train & induce block-wise 
@@ -200,19 +203,13 @@ evaal_imp_approach <- function(path = './Data/Raw/BLCA.Rda', frac_train = 0.75, 
                                    test_pattern = test_pattern)             # Pattern for the test-set
   
   # 1-2 Impute the missing values in the train-set
-  sum(is.na(train_test_bwm$Train$data)) # - 9496920 NAs  -------------------------------------------------------- TO DELETE
-  dim(train_test_bwm$Train$data)        # 232 x 81875
-  
   # 1-2-1 Remove the response variable 'ytarget' from the train-set (temporary for imputation)
   train_ytarget                     <- train_test_bwm$Train$data$ytarget
   train_test_bwm$Train$data$ytarget <- NULL
   
-  # 1-2-2 Do the imputation 
+  # 1-2-2 Do the imputation of missing values in the train-set
   train_test_bwm$Train$data_imputed <- ImputeWithTOBMI(omicsdata = train_test_bwm$Train$data, 
                                                        blockind  = train_test_bwm$Train$block_index) 
-  
-  sum(is.na(train_test_bwm$Train$data_imputed)) # - 0 NAs  -------------------------------------------------------- TO DELETE
-  dim(train_test_bwm$Train$data_imputed)        # 232 x 81874 (as 'y_target' was removed )
   
   # 1-3 Remove the blocks from the imputed train-set that are not available for the test-set
   # 1-3-1 Get the names of the observed blocks in test
@@ -231,27 +228,54 @@ evaal_imp_approach <- function(path = './Data/Raw/BLCA.Rda', frac_train = 0.75, 
     }
   }
   
-  # 1-3-2 Only keep the observed blocks in the test-set
+  # 1-3-2 Remove all blocks & columns from the test-set that are not fully observed 
   blocks_to_keep_ <- which(train_test_bwm$Test$block_names %in% observed_test_blocks)
   cols_to_keep_   <- which(train_test_bwm$Test$block_index %in% blocks_to_keep_)
   y_target_idx_   <- which(colnames(train_test_bwm$Test$data) == "ytarget")
   train_test_bwm$Test$data <- train_test_bwm$Test$data[,c(cols_to_keep_, y_target_idx_)]
   
-  # 1-3-3 Remove all features from the imputed train-set that are not observed in test
+  # 1-3-3 Remove all blocks & columns from the imputed train-set that are not observed in test
   blocks_to_keep <- which(train_test_bwm$Train$block_names %in% observed_test_blocks)
   cols_to_keep   <- which(train_test_bwm$Train$block_index %in% blocks_to_keep)
   train_test_bwm$Train$data_imputed <- train_test_bwm$Train$data_imputed[,cols_to_keep]
   
   # 1-4 Add the original response to the train-set
   train_test_bwm$Train$data_imputed$ytarget <- train_ytarget
+  
+  # 1-5 If the train-set doesn't consist of any observations, return the result
+  #     DF with empty metrics! 
+  if (nrow(train_test_bwm$Train$data_imputed) <= 0) {
+    return(data.frame("path"               = path, 
+                      "frac_train"         = frac_train, 
+                      "split_seed"         = split_seed, 
+                      "block_seed_train"   = block_seed_train,
+                      "block_seed_test"    = block_seed_test, 
+                      "block_order_train_for_BWM" = paste(train_test_bwm$Train$block_names, collapse = ' - '),
+                      "block_order_test_for_BWM"  = paste(train_test_bwm$Test$block_names, collapse = ' - '),
+                      "train_pattern"      = train_pattern, 
+                      "train_pattern_seed" = train_pattern_seed, 
+                      "test_pattern"       = test_pattern, 
+                      "common_blocks"      = paste(observed_test_blocks, collapse = ' - '),
+                      "ntree"              = '---', 
+                      "mtry"               = '---', 
+                      "min_node_size"      = '---', 
+                      "AUC"                = '---',
+                      "Accuracy"           = '---', 
+                      "Sensitivity"        = '---', 
+                      "Specificity"        = '---', 
+                      "Precision"          = '---', 
+                      "Recall"             = '---', 
+                      "F1"                 = '---', 
+                      "BrierScore"         = '---'))
+  }
 
-  # [2] Fit an RF on the imputed train-set, get predictions for the test-set & get metrics
-  # 2-1 Train a RF on the imputed train set & use it to predict on the test-set then!
+  # [2] Fit an RF on the imputed train-set, get predictions for the test-set & calc metrics
+  # 2-1 Train a RF on the imputed train-set & use it to predict on the test-set then!
   preds_test_set <- get_predicition(train = train_test_bwm$Train$data_imputed,
                                     test = train_test_bwm$Test$data)
   
   # 2-2 Calculate the metrics based on the true & predicted labels
-  # 2-2-1  Confusion Matrix & all corresponding metrics (Acc, F1, Precision, ....)
+  # 2-2-1 Confusion Matrix & all corresponding metrics (Acc, F1, Precision, ....)
   metrics_1 <- caret::confusionMatrix(preds_test_set$pred_classes, 
                                       factor(train_test_bwm$Test$data$ytarget, 
                                              levels = c(0, 1)),
@@ -290,14 +314,22 @@ evaal_imp_approach <- function(path = './Data/Raw/BLCA.Rda', frac_train = 0.75, 
 }
 
 # [1] Run the experiments                                                    ----
-# 1-1 Initalize a empty DF to store the results
+# 1-1 Initialize a empty DF to store the results
 IMP_res <- data.frame()
 
-# 1-2 Define a list with the paths to the availabe DFs
+# 1-2 Define a list with the paths to the available DFs
 df_paths <- paste0("./Data/Raw/", list.files("./Data/Raw/"))
 
-# 1-3 Loop over all the possible settings for the evaluation of the IMP-Approach
-#     each setting is evaluated 5-times!
+# 1-3 Create a list of seeds for each single evaluation-setting
+set.seed(1234)
+count    <- 1
+allseeds <- base::sample(1000:10000000, 
+                         size = length(df_paths) * length(c(1, 2, 3, 4, 5)) * 
+                                length(c(1, 2, 3, 4)) * length(c(1, 2, 3, 4, 5)))
+
+# 1-4 Evaluate a RF on all the possible combinations of block-wise missingness
+#     patterns in train- & test-set for all DFs in 'df_paths'. Each is evaluated
+#     5-times.
 for (curr_path in df_paths) {
   for (curr_train_pattern in c(1, 2, 3, 4, 5)) {
     for (curr_test_pattern in c(1, 2, 3, 4)) {
@@ -309,15 +341,26 @@ for (curr_path in df_paths) {
             "Current Test Patter:   >", curr_test_pattern, '\n',
             "Current Repetition:    >", curr_repetition, '\n')
         
-        # Set the seed for the 'split'
-        curr_split_seed = 12345678 + curr_repetition
+        # Get initial seed for the current combination evaluation-settings
+        int_seed <- allseeds[count]
+        count    <- count + 1
         
-        # Set the seed for the shuffling of the blocks of train & test
-        curr_block_seed_train = 1234567 + curr_repetition
-        curr_block_seed_test  = 7654321 + curr_repetition
+        # Set seed & draw points from uniform distribution
+        set.seed(int_seed)    
+        seeds <- round(runif(4, 0, 100000))
         
-        # Set the seed for the train_pattern (shuffling of observations)
-        curr_train_pattern_seed = 12345 + curr_repetition
+        # Use these 'seeds' to set the four necessary seeds for the evaluation:
+        #     1. Seed to split data into test & train
+        curr_split_seed <- seeds[1]
+        
+        #     2. Seed to shuffle the block order in 'train'
+        curr_block_seed_train <- seeds[2]
+        
+        #     3. Seed to shuffle the block order in 'test'
+        curr_block_seed_test <- seeds[3]
+        
+        #     4. Seed for the train-pattern (assignment of obs. in train to folds)
+        curr_train_pattern_seed <- seeds[4]
         
         # Run the evaluation with current settings
         curr_res <- tryCatch(evaal_imp_approach(path               = curr_path, 
@@ -354,10 +397,11 @@ for (curr_path in df_paths) {
                              }
         ) 
         
-        # Add the curr_repetition to 'curr_res', before adding it to 'IMP_res'
+        # Add the, 'int_seed', 'curr_repetition' & 'SingleBlock' to 'curr_res'
+        curr_res$int_seed   <- int_seed
         curr_res$repetition <- curr_repetition
         curr_res$approach   <- 'Imputation'
-        
+
         # Add the results of the setting to 'IMP_res' & save it
         IMP_res <- rbind(IMP_res, curr_res)
         write.csv(IMP_res, './Docs/Evaluation_Results/IMP_Approach/IMP_Eval.csv')
